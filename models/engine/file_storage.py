246bf5309167bdb37e454feae7cb471314ab35cd
Ahmad <ahmad.new.m.v@gamil.com>
2024-05-14 20:46:01 +0000

    Added new features

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,9 +7,21 @@ class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
+    classes = ['BaseModel', 'User', 'State', 'City', 'Amenity',
+               'Place', 'Review']
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls is not None and type(cls) is not type:
+            raise TypeError('"cls" must a class or None')
+        if cls is not None and cls.__name__ not in FileStorage.classes:
+            raise ValueError(f'{cls} does not exist')
+        if cls is not None:
+            filtered_dict = {}
+            for key in FileStorage.__objects.keys():
+                if cls.__name__ in key:
+                    filtered_dict[key] = FileStorage.__objects[key]
+            return filtered_dict
         return FileStorage.__objects
 
     def new(self, obj):
@@ -45,6 +57,13 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        if obj:
+            cls_name = obj.__class__.__name__
+            if cls_name not in FileStorage.classes:
+                raise TypeError('obj is not valid')
+            del self.__objects[f'{cls_name}.{obj.id}']
