e6fc22788cba9e36e629f87ca5a8aeb441dc960e
Ahmad <ahmad.new.m.v@gamil.com>
2024-05-18 18:23:28 +0000

    Added flexibility

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage or DBStorage"""
 from os import getenv
-
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.user import User
+from models.amenity import Amenity
+from models.review import Review
 
 if getenv("HBNB_TYPE_STORAGE") == "db":
     from models.engine.db_storage import DBStorage
