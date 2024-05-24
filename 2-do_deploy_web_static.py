8bed835314a361b0de44e0ac690fcfb1e01fcd95
Ahmad <ahmad.new.m.v@gamil.com>
2024-05-24 18:18:53 +0000

    Changed the implementation

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -37,24 +37,26 @@ def do_deploy(archive_path):
     result = put(local_path=archive_path, remote_path="/tmp/")
     if result.failed:
         return False
-    result = run(f"tar -xzf /tmp/{archive_name} -C /data/web_static/releases/")
+    result = run(f"mkdir -p /data/web_static/releases/{foldername}")
     if result.failed:
         return False
-    new_name = f"/data/web_static/releases/{foldername}"
-    if foldername != "web_static":
-        result = run(f"mv /data/web_static/releases/web_static {new_name}")
-        if result.failed:
-            return False
+    result = run(f"tar -xzf /tmp/{archive_name} -C /data/web_static/releases/{foldername}/")
+    if result.failed:
+        return False
     result = run(f"rm /tmp/{archive_name}")
     if result.failed:
         return False
-    result = run("rm -r /data/web_static/current")
+    result = run(f"mv /data/web_static/releases/{foldername}/web_static/* /data/web_static/releases/{foldername}")
     if result.failed:
         return False
-    result = run(f"ln -sf {new_name} /data/web_static/")
+    result = run(f"rm -rf /data/web_static/releases/{foldername}/web_static")
     if result.failed:
         return False
-    result = run(f"mv /data/web_static/{foldername} /data/web_static/current")
+    result = run("rm -rf /data/web_static/current")
     if result.failed:
         return False
+    result = run(f"ln -s /data/web_static/releases{foldername}/ /data/web_static/current")
+    if result.failed:
+        return False
+    print("New version deployed!")
     return True
