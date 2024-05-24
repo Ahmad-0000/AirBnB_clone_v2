64a7ad24d8c4f4af0894fa3ca4c1d6f802a38022
Ahmad <ahmad.new.m.v@gamil.com>
2024-05-24 13:10:49 +0000

    Added the solution of the 2nd task

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Main fabric file to deploy my static website"""
+from fabric.api import *
+from datetime import datetime
+
+env.hosts = ['localhost']
+
+now = datetime.now()
+year = str(now.year)
+month = str(now.month)
+day = str(now.day)
+hour = str(now.hour)
+minute = str(now.minute)
+second = str(now.second)
+
+name = year + month + day + hour + minute + second
+
+
+def do_pack():
+    """Making a .tgz archive from the static website directory"""
+    local("mkdir -p versions")
+    local(f"tar -czvf {name}.tgz web_static")
