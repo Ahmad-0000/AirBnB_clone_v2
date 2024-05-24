79673632de2bc6eb591ed50145e69f31c67ada93
Ahmad <ahmad.new.m.v@gamil.com>
2024-05-24 12:35:59 +0000

    Cautious changes

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,15 +4,14 @@
 nginx_config="events {}\n\nhttp {\n\tserver {\n\t\tlisten 80;\n\t\tlisten [::]:80;\n\n\t\tserver_name ahmad-basheer.tech;\n\n\t\tlocation /hbnb_static {\n\t\t\talias /data/web_static/current;\n\t\t}\n\t}\n}"
 
 # Installing Nginx
-apt-get -y update
 apt-get -y install nginx
 
 # Creating main "web_static" in the file system
-mkdir /data
-mkdir /data/web_static
-mkdir /data/web_static/releases
-mkdir /data/web_static/releases/test
-mkdir /data/web_static/shared
+mkdir -p /data
+mkdir -p /data/web_static
+mkdir -p /data/web_static/releases
+mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/shared
 echo "Configuration test: Success!" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data
