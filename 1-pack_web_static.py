628a75e6ff1216dcc9d394332356dfb999c28e20
Ahmad <ahmad.new.m.v@gamil.com>
2024-05-24 18:41:22 +0000

    Added some caution steps

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -18,6 +18,14 @@ name = year + month + day + hour + minute + second
 
 def do_pack():
     """Making a .tgz archive from the static website directory"""
-    local("mkdir -p versions")
-    local(f"tar -czvf web_static_{name}.tgz web_static")
-    local(f"mv web_static_{name}.tgz versions/")
+    result = local("mkdir -p versions")
+    if result.failed:
+        return None
+    result = local(f"tar -czvf web_static_{name}.tgz web_static")
+    if result.failed:
+        return None
+    result = local(f"mv web_static_{name}.tgz versions/")
+    if result.failed:
+        return None
+    return f"versions/web_static_{name}.tgz"
+
