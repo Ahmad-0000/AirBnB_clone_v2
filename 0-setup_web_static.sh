b890134d4e50f223fabac33cc80dfa4d89ab3c60
Ahmad <ahmad.new.m.v@gamil.com>
2024-05-24 12:09:52 +0000

    Added the solution of the 1st task

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# A script that sets up my web servers for the deployment of "web_static"
+
+nginx_config="events {}\n\nhttp {\n\tserver {\n\t\tlisten 80;\n\t\tlisten [::]:80;\n\n\t\tserver_name ahmad-basheer.tech;\n\n\t\tlocation /hbnb_static {\n\t\t\talias /data/web_static/current;\n\t\t}\n\t}\n}"
+
+# Installing Nginx
+apt-get update
+apt-get upgrade
+apt-get -y install nginx
+
+# Creating main "web_static" in the file system
+mkdir /data
+mkdir /data/web_static
+mkdir /data/web_static/releases
+mkdir /data/web_static/releases/test
+mkdir /data/web_static/shared
+echo "Configuration test: Success!" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data
+
+# Configuring Nginx to serve content of "/data/web_static/current" to
+# http://ahmad-basheer.tech/hbnb_static
+cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
+echo -e "$nginx_config" > /etc/nginx/nginx.conf
+nginx -s reload
