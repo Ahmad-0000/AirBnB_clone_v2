007c96ae1e6f2cbbe15e24355f27d24a75677290
Ahmad <ahmad.new.m.v@gmail.com>
2024-06-02 22:14:44 +0200

    Changed some implementation

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""
+'''
 A script to render a specific page when visiting the route "/states_list"
-"""
+'''
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -10,21 +10,24 @@ from models.state import State
 app = Flask(__name__)
 all_states = storage.all(State)
 
-@app.route("/states_list", strict_slashes=False)
-def states():
-    """
-    A function to render "templates/7-states_list.html" when visiting the
-    route "/states_list"
-    """
-    return render_template('7-states_list.html', all_states=all_states)
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """
+def teardown(exception):
+    '''
     A function to ensure the proper closing of the database connection after
     each serve
-    """
+    '''
     storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states():
+    '''
+    A function to render "templates/7-states_list.html" when visiting the
+    route "/states_list"
+    '''
+    return render_template('7-states_list.html', all_states=all_states)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
