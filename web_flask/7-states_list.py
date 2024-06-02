777e3a578914863a41afbdde06e31a27d4f47323
Ahmad <ahmad.new.m.v@gmail.com>
2024-06-02 20:48:04 +0200

    Added a feature
    Allowed for rendering a template containing all the states
    in the database

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+A script to render a specific page when visiting the route "/states_list"
+"""
+from flask import Flask, render_template
+from web_flask import storage
+from web_flask import State
+
+
+app = Flask(__name__)
+all_states = storage.all(State)
+
+@app.route("/states_list", strict_slashes=False)
+def states():
+    """
+    A function to render "templates/7-states_list.html" when visiting the
+    route "/states_list"
+    """
+    return render_template('7-states_list.html', all_states=all_states)
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """
+    A function to ensure the proper closing of the database connection after
+    each serve
+    """
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
