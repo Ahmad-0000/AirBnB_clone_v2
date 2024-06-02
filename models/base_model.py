4f1106a635933189fb0d84f25e5aa30cb1619878
root <root@localhost.localdomain>
2024-06-02 18:37:43 +0200

    Modified "__str__" method

--- models/base_model.py
+++ models/base_model.py
@@ -42,7 +42,13 @@ class BaseModel:
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        less_self_dict = {}
+        for key in self.__dict__.keys():
+            if key == '_sa_instance_state':
+                pass
+            else:
+                less_self_dict[key] = self.__dict__[key]
+        return '[{}] ({}) {}'.format(cls, self.id, less_self_dict)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
