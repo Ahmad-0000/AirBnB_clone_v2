165519b1cf2cbf628e397fdfc641ae3378a6ed53
Ahmad <ahmad.new.m.v@gmial.com>
2024-06-02 14:44:06 +0200

    Conformed to PEP 1.8.*

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -69,7 +69,8 @@ def number_template_route(n):
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even_route(n):
     """
-    Rendering custom template when visiting the route /number_odd_or_even/<int:n>
+    Rendering custom template when visiting the route 
+    /number_odd_or_even/<int:n>
     """
     return render_template("6-number_odd_or_even.html", n=n)
 
